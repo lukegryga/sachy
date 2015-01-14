8f1a5418ad9a98fea5ce8d1b5846401bbf176f87
Lukáš <Luk@lukebook>
2015-01-14 11:29:39 +0100

    Initial commit

---
+++ src/sachy/Sachy.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sachy;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author Lukáš
+ */
+public class Sachy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
