6f08f89d160faa8ca37a784f9061c25e4c7712d9
Lukáš <Luk@lukebook>
2015-01-16 15:57:49 +0100

    Přidání zbytečného komentáře

--- src/sachy/Pesec.java
+++ src/sachy/Pesec.java
@@ -17,15 +17,12 @@ public class Pesec extends Figura{
         super(pozice, barva);
     }
     
-    boolean pohniSe(Point kam) throws Exception{
+    boolean jeTahPovolen(Point kam) throws Exception{
         int psX = kam.x - pozice.x;
         int psY = kam.y - pozice.y;
         if(barva == true){
-            if(pozice.y = 2){
-                if(((psY == 1 || psY == 2 ) && psY == 0 ) ||
-                        (psX == 1 || psX == -1) && psY == 1 && sachovnice.jeVolno(pozice) == 0);
-            }
-        }
+            
+        }return false;
     }
     
 }
