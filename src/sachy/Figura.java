d3e2c4ea651fc74d130293eea11ae0968d3a0f6a
Lukáš <Luk@lukebook>
2015-01-16 15:58:06 +0100

    Přidání zbytečného komentáře

--- src/sachy/Figura.java
+++ src/sachy/Figura.java
@@ -2,12 +2,15 @@
 package sachy;
 
 import java.awt.Point;
+import java.util.ArrayList;
 
 public abstract class Figura 
 {
     protected final Sachovnice sachovnice = Sachovnice.getSachovnice();
     protected final boolean barva;
     protected Point pozice;
+    protected boolean vyhozena = false;
+    protected ArrayList<Point> mnozinaTahu = new ArrayList<>();
     
     public Figura(Point pozice, boolean barva){
         this.pozice = pozice;
--- src/sachy/Sachovnice.java
+++ src/sachy/Sachovnice.java
@@ -11,6 +11,8 @@ public class Sachovnice {
     private static Sachovnice jedinacek = null;
     
     private Figura[][] rozmisteni;
+    private boolean hra = false;
+    private boolean jeNaTahu = true;
     
     public static Sachovnice getSachovnice(){
         if(jedinacek == null){
@@ -21,14 +23,32 @@ public class Sachovnice {
     
     private Sachovnice(){}
     
+    public boolean tahni(){
+        return false;
+        
+    }
+    
     int jeVolno(Point pozice){
         if(rozmisteni[pozice.x-1][pozice.y-1] == null){
             return -1;
         }else if(rozmisteni[pozice.x-1][pozice.y-1].barva){
-            
             return 1;
         }
         return 0;
+    }
+    
+    static Point prevedNaSouradnice(String tah){
+        tah = tah.toLowerCase();
+        char[] pozice = tah.toCharArray();
+        if(pozice.length != 2){
+            throw new IllegalArgumentException("Neplanty řetězec");
+        }
+        int x = pozice[0] - 96;
+        int y = pozice[1] - '0';
+        if((x >= 9 || x <= 0) || (y >= 9 || y <= 0)){
+            throw new IllegalArgumentException("Neplatne souřadnice");
+        }
+        return new Point(x, y);
     }
     
 }
